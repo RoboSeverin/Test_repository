00f28e9e31b91f01012b026ed3c4859896825e3a
RoboSeverin <soren@lystlund.dk>
2025-06-18 11:53:47 +0200

    [UPDATE] Created continously input functionality

--- test_script.py
+++ test_script.py
@@ -7,4 +7,18 @@ def calculate_area_cli():
 
     print(f"The area of a cirlce with radius {radius} is {area}")
 
+def proceed_prompt():
+    proceed_bool : bool = False
+    proceed_str = input("Want to continue? y/n\n")
+    if proceed_str == "y" or proceed_str == "Y":
+        proceed_bool = True
+    return proceed_bool
+
+
 calculate_area_cli()
+proceed = proceed_prompt()
+
+while proceed:
+    calculate_area_cli()
+    proceed = proceed_prompt()
+
